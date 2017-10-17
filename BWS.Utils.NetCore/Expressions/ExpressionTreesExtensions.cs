6a545605349ef400ed5ddf1ef4ed156638a509ae
Wallace Sun <miao17game@qq.com>
2017-10-17 23:27:42 +0800

    update fix for ExpressionTreeExtensions

--- BWS.Utils.NetCore/Expressions/ExpressionTreesExtensions.cs
+++ BWS.Utils.NetCore/Expressions/ExpressionTreesExtensions.cs
@@ -5,10 +5,28 @@ using System.Text;
 
 namespace BWS.Utils.NetCore.Expressions {
 
+    internal class ParameterReplacer : ExpressionVisitor {
+
+        private readonly ParameterExpression _parameter;
+
+        protected override Expression VisitParameter(ParameterExpression node) {
+            return base.VisitParameter(_parameter);
+        }
+
+        internal ParameterReplacer(ParameterExpression parameter) {
+            _parameter = parameter;
+        }
+
+    }
+
     /// <summary>
     /// ExpressionTree Extensions.
     /// </summary>
     public static class ExpressionTreesExtensions {
+
+        private static BinaryExpression CreateVisit<T>(Expression exp) {
+            return (BinaryExpression)new ParameterReplacer(Expression.Parameter(typeof(T))).Visit(exp);
+        }
 
         /// <summary>
         /// Union the filter with And method.
@@ -17,8 +35,11 @@ namespace BWS.Utils.NetCore.Expressions {
         /// <param name="expr1"></param>
         /// <param name="expr2"></param>
         /// <returns></returns>
-        public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2) 
-            => Expression.Lambda<Func<T, bool>>(Expression.AndAlso(expr1.Body, expr2.Body), expr1.Parameters);
+        public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2) {
+            var param = Expression.Parameter(typeof(T));
+            var body = Expression.AndAlso(expr1.Body, expr2.Body);
+            return Expression.Lambda<Func<T, bool>>(CreateVisit<T>(body), param);
+        }
 
         /// <summary>
         /// Filt the filter with Or method.
@@ -27,8 +48,11 @@ namespace BWS.Utils.NetCore.Expressions {
         /// <param name="expr1"></param>
         /// <param name="expr2"></param>
         /// <returns></returns>
-        public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2) 
-            => Expression.Lambda<Func<T, bool>>(Expression.OrElse(expr1.Body, expr2.Body), expr1.Parameters);
+        public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2) {
+            var param = Expression.Parameter(typeof(T));
+            var body = Expression.OrElse(expr1.Body, expr2.Body);
+            return Expression.Lambda<Func<T, bool>>(CreateVisit<T>(body), param);
+        }
         
     }
 }
